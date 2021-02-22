687f8b3a1bc482269d5e6a521205453d55ae498a
Jay Bendre <jay@ubuntu1804.linuxvmimages.local>
2021-02-22 10:40:23 -0500

    Changes

--- LMS/settings.py
+++ LMS/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'LMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -89,7 +90,14 @@ DATABASES = {
         },
     }
 }
+"""
 
+DATABASES = {
+	"default":{
+		"ENGINE": "django.db.backends.sqlite3",
+		"NAME" : "db.sqlite3"
+		}
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
