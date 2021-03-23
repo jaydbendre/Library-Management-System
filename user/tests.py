585782c4a162ea837e9d52f07407190a2d8d132e
jaydbendre <2017.jay.bendre@ves.ac.in>
2021-03-23 06:22:41 -0400

    Selenium

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-# Create your tests here.
+class LoginTest(LiveServerTestCase):
+	def setUp(self):
+		self.selenium = webdriver.Firefox()
+		super(LoginTest,self).setUp()
+
+	def tearDown(self):
+		self.selenium.quit()
+		super(LoginTest,self).tearDown()
+
+	def test_login(self):
+		selenium = self.selenium
+		selenium.get("http://127.0.0.1:8000")
+		selenium.find_element(By.className("btn-gradient")).click()
+
+
